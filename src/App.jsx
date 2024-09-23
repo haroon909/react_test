ada73b01017b3e15b91f0dc7b3186011a76c28f3
haroon909 <aharoon909@gmail.com>
2024-09-23 12:40:30 +0500

    --- Auto Git Commit ---

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
  import Navbar from './components/Navbar'
  import { BrowserRouter, Routes, Route } from 'react-router-dom'
  import Registration from './Pages/Registration'
-import Login from './Pages/Login'
+// import Login from './Pages/Login'
 
 
 const App = () => {
